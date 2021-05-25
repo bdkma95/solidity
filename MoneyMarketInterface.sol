4078b1d842d1dfde1e4a1a8d2bbf880c87e4100c
bdkma95 <68553253+bdkma95@users.noreply.github.com>
2021-05-25 11:59:06 +0200

    Create MoneyMarketInterface.sol

---
+++ MoneyMarketInterface.sol
@@ -0,0 +1,23 @@
+  
+pragma solidity ^0.4.24;
+
+contract MoneyMarketInterface {
+  uint public collateralRatio;
+  address[] public collateralMarkets;
+
+  function borrow(address asset, uint amount) public returns (uint);
+
+  function supply(address asset, uint amount) public returns (uint);
+
+  function withdraw(address asset, uint requestedAmount) public returns (uint);
+
+  function repayBorrow(address asset, uint amount) public returns (uint);
+
+  function getSupplyBalance(address account, address asset) view public returns (uint);
+
+  function getBorrowBalance(address account, address asset) view public returns (uint);
+
+  function assetPrices(address asset) view public returns (uint);
+
+  function calculateAccountValues(address account) view public returns (uint, uint, uint);
+}
