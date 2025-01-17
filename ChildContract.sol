a39bb3d51421a090cd9d404db1cd2f94444346c0
Boris <68553253+bdkma95@users.noreply.github.com>
2025-01-17 21:11:52 +0100

    Update ChildContract.sol
    Security Enhancements: Added ReentrancyGuard for safe fund withdrawals.
    Gas Optimization: Combined mappings for promo codes and reduced unnecessary state variables.
    Improved Metadata Management: Optimized tokenURI logic for efficient metadata management and updated function naming for clarity.
    Minting Logic: Optimized minting logic and validation checks for more clarity.

--- ChildContract.sol
+++ ChildContract.sol
@@ -1,200 +1,123 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0 <0.9.0;
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+pragma solidity ^0.8.14;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract ChildContract is ERC721, Ownable {  
-  using Strings for uint256;  
-  using Counters for Counters.Counter;
-  
-  Counters.Counter private supply;
-  
-  string public uriPrefix = "";  
-  string public uriSuffix = ".json";  
-  string public hiddenMetadataUri;  
-  string public collectionName;  
-  string public collectionSymbol;  
-  string public collectionDescription;    
-  
-  uint256 public cost = 0.01 ether;  
-  uint256 public maxSupply = 10000;  
-  uint256 public maxMintAmountPerTx = 5;  
-  uint256 public collectionPrice;  
-  uint256 public collectionElements;
-  
-  bool public paused = true;  
-  bool public revealed = false;
-  
-  struct Ben {    
-    string collectionName;    
-    string collectionSymbol;    
-    string collectionDescription;    
-    uint256 collectionPrice;    
-    uint256 collectionElements;  
-  }
-    
-  mapping(uint => string) public codePromo;
-  
-  struct Time {    
-    uint startTime;    
-    uint deadline;  
- }
- 
-  mapping(string => Time) public _time;
-  
-  Ben[] public ben;
-  
-  address[] public deployedContract;  
-  string[] public promoCode;
-  
-  constructor(string memory _collectionName, string memory _collectionSymbol, string memory _collectionDescription, uint _collectionPrice, uint _collectionElements) ERC721("NAME", "SYMBOL") {    
-    setHiddenMetadataUri("ipfs://__CID__/hidden.json");    
-    collectionName = _collectionName;    
-    collectionSymbol = _collectionSymbol;    
-    collectionDescription = _collectionDescription;    
-    collectionPrice = _collectionPrice;    
-    collectionElements = _collectionElements;
-    
-    Ben memory benInitial = Ben({
-    
-      collectionName: _collectionName,      
-      collectionSymbol: _collectionSymbol,      
-      collectionDescription: _collectionDescription,      
-      collectionPrice: _collectionPrice,      
-      collectionElements: _collectionElements    
-    });
-    
-    ben.push(benInitial);    
-    deployedContract.push(address(this));  
-  }
-  
-  modifier mintCompliance(uint256 _mintAmount) {    
-    require(_mintAmount > 0 && _mintAmount <= maxMintAmountPerTx, "Invalid mint amount!");    
-    require(supply.current() + _mintAmount <= maxSupply, "Max supply exceeded!");    
-    _;  
-  }
-  
-  function totalSupply() public view returns (uint256) {
-    return supply.current();  
-  }
-  
-  function mint(uint256 _mintAmount) public payable mintCompliance(_mintAmount) {    
-    require(!paused, "The contract is paused!");    
-    require(msg.value >= cost * _mintAmount, "Insufficient funds!");
-    
-    _mintLoop(msg.sender, _mintAmount);  
-  }    
-  
-  function mintForAddress(uint256 _mintAmount, address _receiver) public mintCompliance(_mintAmount) onlyOwner {    
-  _mintLoop(_receiver, _mintAmount);  
-  }
-  
-  function walletOfOwner(address _owner) 
-    public 
-    view 
-    returns (uint256[] memory)  
-  {    
-  uint256 ownerTokenCount = balanceOf(_owner);    
-  uint256[] memory ownedTokenIds = new uint256[](ownerTokenCount);    
-  uint256 currentTokenId = 1;    
-  uint256 ownedTokenIndex = 0;
-  
-  while (ownedTokenIndex < ownerTokenCount && currentTokenId <= maxSupply) {
-    address currentTokenOwner = ownerOf(currentTokenId);
-    
-    if (currentTokenOwner == _owner) {        
-      ownedTokenIds[ownedTokenIndex] = currentTokenId;
-      
-      ownedTokenIndex++;      
-    }
-    
-    currentTokenId++;    
-  }
-    
-  return ownedTokenIds;  
- }
+contract ChildContract is ERC721Enumerable, Ownable, ReentrancyGuard {
+    using Counters for Counters.Counter;
+    using Strings for uint256;
 
-  function tokenURI(uint256 _tokenId)    
-    public    
-    view    
-    virtual    
-    override    
-    returns (string memory)  
-  {    
-    require(      
-      _exists(_tokenId),
-      "ERC721Metadata: URI query for nonexistent token"    
-    );
-    
-    if (revealed == false) {
-      return hiddenMetadataUri;
+    Counters.Counter private supply;
+
+    string public uriPrefix = "";
+    string public uriSuffix = ".json";
+    string public hiddenMetadataUri;
+
+    uint256 public cost = 0.01 ether;
+    uint256 public maxSupply = 10000;
+    uint256 public maxMintAmountPerTx = 5;
+
+    bool public paused = true;
+    bool public revealed = false;
+
+    struct Time {
+        uint startTime;
+        uint deadline;
     }
-    
-    string memory currentBaseURI = _baseURI();    
-    return bytes(currentBaseURI).length > 0        
-      ? string(abi.encodePacked(currentBaseURI, _tokenId.toString(), uriSuffix))
-      : "";  
-  }
-  
-  function setRevealed(bool _state) public onlyOwner {
-    revealed = _state;  
-  }
-  
-  function setCost(uint256 _cost) public onlyOwner {    
-    cost = _cost;  
-  }
-  
-  function setMaxMintAmountPerTx(uint256 _maxMintAmountPerTx) public onlyOwner {    
-    maxMintAmountPerTx = _maxMintAmountPerTx;  
-  }
-  
-  function setHiddenMetadataUri(string memory _hiddenMetadataUri) public onlyOwner {    
-    hiddenMetadataUri = _hiddenMetadataUri;  
-  }
-  
-  function setUriPrefix(string memory _uriPrefix) public onlyOwner {    
-    uriPrefix = _uriPrefix;  
-  }
-  
-  function setUriSuffix(string memory _uriSuffix) public onlyOwner {    
-    uriSuffix = _uriSuffix;  
-  }
-  
-  function setPaused(bool _state) public onlyOwner {    
-    paused = _state;  
-  }
-  
-  function withdraw() public onlyOwner {    
-    (bool os, ) = payable(owner()).call{value: address(this).balance}("");    
-    require(os);  
-  }
-  
-  function setCodePromo(string memory _newCodePromo, uint _startTime, uint _endTime) public onlyOwner {        
-    
-    _time[_newCodePromo] = Time({      
-      startTime: uint(_startTime),      
-      deadline : uint(_endTime)    
-    });    
-    codePromo[0] = _newCodePromo;    
-    promoCode.push(_newCodePromo);  
-  }
-  
-  function getCodePromo() public view returns (string[] memory) {    
-    return promoCode;  
-  }
-  
-  function getTimeCodePromo(string memory _code) public view returns (Time memory) {    
-    return _time[_code];  
-  }
-  
-  function _mintLoop(address _receiver, uint256 _mintAmount) internal {    
-    for (uint256 i = 0; i < _mintAmount; i++) {      
-      supply.increment();      
-      _safeMint(_receiver, supply.current());    
-    }  
-  }
-  function _baseURI() internal view virtual override returns (string memory) {    
-    return uriPrefix;  
-  }
+
+    mapping(string => Time) public promoCodes;
+
+    constructor(
+        string memory _collectionName,
+        string memory _collectionSymbol,
+        string memory _collectionDescription
+    ) ERC721(_collectionName, _collectionSymbol) {
+        hiddenMetadataUri = "ipfs://__CID__/hidden.json";
+    }
+
+    modifier mintCompliance(uint256 _mintAmount) {
+        require(_mintAmount > 0 && _mintAmount <= maxMintAmountPerTx, "Invalid mint amount!");
+        require(supply.current() + _mintAmount <= maxSupply, "Max supply exceeded!");
+        _;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return supply.current();
+    }
+
+    function mint(uint256 _mintAmount) public payable mintCompliance(_mintAmount) {
+        require(!paused, "The contract is paused!");
+        require(msg.value >= cost * _mintAmount, "Insufficient funds!");
+
+        _mintLoop(msg.sender, _mintAmount);
+    }
+
+    function mintForAddress(uint256 _mintAmount, address _receiver) public mintCompliance(_mintAmount) onlyOwner {
+        _mintLoop(_receiver, _mintAmount);
+    }
+
+    function walletOfOwner(address _owner) public view returns (uint256[] memory) {
+        uint256 ownerTokenCount = balanceOf(_owner);
+        uint256[] memory ownedTokenIds = new uint256[](ownerTokenCount);
+
+        for (uint256 i = 0; i < ownerTokenCount; i++) {
+            ownedTokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+        }
+
+        return ownedTokenIds;
+    }
+
+    function tokenURI(uint256 _tokenId) public view virtual override returns (string memory) {
+        require(_exists(_tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+        if (!revealed) {
+            return hiddenMetadataUri;
+        }
+
+        return string(abi.encodePacked(uriPrefix, _tokenId.toString(), uriSuffix));
+    }
+
+    function setRevealed(bool _state) public onlyOwner {
+        revealed = _state;
+    }
+
+    function setCost(uint256 _cost) public onlyOwner {
+        cost = _cost;
+    }
+
+    function setMaxMintAmountPerTx(uint256 _maxMintAmountPerTx) public onlyOwner {
+        maxMintAmountPerTx = _maxMintAmountPerTx;
+    }
+
+    function setPaused(bool _state) public onlyOwner {
+        paused = _state;
+    }
+
+    function withdraw() public onlyOwner nonReentrant {
+        (bool os, ) = payable(owner()).call{value: address(this).balance}("");
+        require(os, "Withdraw failed");
+    }
+
+    function setCodePromo(string memory _newCodePromo, uint _startTime, uint _endTime) public onlyOwner {
+        promoCodes[_newCodePromo] = Time({ startTime: _startTime, deadline: _endTime });
+    }
+
+    function getCodePromo(string memory _code) public view returns (Time memory) {
+        return promoCodes[_code];
+    }
+
+    function _mintLoop(address _receiver, uint256 _mintAmount) internal {
+        for (uint256 i = 0; i < _mintAmount; i++) {
+            supply.increment();
+            _safeMint(_receiver, supply.current());
+        }
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return uriPrefix;
+    }
 }
